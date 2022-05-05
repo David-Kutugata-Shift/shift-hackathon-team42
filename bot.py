c01759f0a28f829ab3bb9200b3735919c5c80d52
Guilherme Wanderley <guilherme.wanderley@shift-technology.com>
2022-05-05 15:16:43 +0200

    Adds query engine

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
 import slack
 import os
+import numpy as np
+
 from pathlib import Path
 from dotenv import load_dotenv
 from flask import Flask
 from slackeventsapi import SlackEventAdapter
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -25,8 +30,8 @@ def get_conversation_id(conversation_name):
                 if channel["name"] == conversation_name:
                     conversation_id = channel["id"]
                     return conversation_id
-    except SlackApiError as e:
-        print(f"Error: {e}")
+    except:
+        print(f"Error")
 
 def get_messages_history(conversation_id):
     try:
@@ -35,14 +40,31 @@ def get_messages_history(conversation_id):
         # These results are paginated, see: https://api.slack.com/methods/conversations.history$pagination
         result = client.conversations_history(channel=conversation_id)
         return result["messages"]
-    except SlackApiError as e:
-        print("Error creating conversation: {}".format(e))
+    except:
+        print("Error creating conversation")
+
+def tfidf(msgs, vectorizer=None):
+    if vectorizer is None:
+        vectorizer = TfidfVectorizer(min_df=10)
+    db_rep = vectorizer.fit_transform(msgs)
+    return db_rep, vectorizer
+
+def retrieve_similars_idxs(question_rep, db_rep, n=5):
+    best_matches = cosine_similarity(question_rep, db_rep)
+    best_matches_idx = np.argsort(best_matches)
+    return best_matches_idx[1, n+1]
+
+def get_similar_msgs(question, msgs, db_rep, vectorizer):
+    question_rep, _ = vectorizer.fit_transform(question)
+    idxs = retrieve_similars_idxs(question_rep, db_rep)
+    return msgs[idxs]
 
 def init_db():
     conv_id = get_conversation_id('#hackathon')
     msgs = get_messages_history(conv_id)
     print(len(msgs), conv_id)
-    return msgs
+    db_rep, vectorizer = tfidf(msgs)
+    return db_rep, msgs, vectorizer
 
 def is_question(text):
     if text[-1] == '?':
@@ -57,9 +79,13 @@ def message(payload):
     text = event.get('text')
 
     if user_id != BOT_ID:
-        client.chat_postMessage(channel='#hackathon', text='hi ' + user_id + ', great question')
-
+        if is_question(text):
+            print('Received a question. Querying db:')
+            sim_msgs = get_similar_msgs(text, msgs, db_rep, vectorizer)
+            for msg in sim_msgs:
+                client.chat_postMessage(channel='#hackathon', text=msg)
 
 if __name__ == "__main__":
-    db = init_db()
+    db_rep, msgs, vectorizer = init_db()
+
     app.run(debug=True)
